d4fbd969f34d328526478cdf0a10601c745e370f
İbrahim DOĞAN <33070834+ibrahimdoqn@users.noreply.github.com>
2022-06-09 14:07:54 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,6 +25,25 @@ function solÖn (hiz: number) {
         pins.digitalWritePin(DigitalPin.P14, 0)
     }
 }
+input.onButtonPressed(Button.A, function () {
+    sagÖn(1)
+    SolArka(-1)
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "SagOn") {
+        sagÖn(value)
+    } else if (name == "SolOn") {
+        solÖn(value)
+    } else if (name == "SolArka") {
+        SolArka(value)
+    } else if (name == "SagArka") {
+        sagArka(value)
+    } else if (name == "Dur") {
+        dur(1)
+    } else {
+    	
+    }
+})
 function dur (hiz: number) {
     pins.digitalWritePin(DigitalPin.P12, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
@@ -44,13 +63,7 @@ function sagArka (hiz: number) {
         pins.digitalWritePin(DigitalPin.P7, 0)
     }
 }
+radio.setGroup(1)
 led.enable(false)
 pins.analogWritePin(AnalogPin.P8, 1023)
 pins.analogWritePin(AnalogPin.P15, 1023)
-solÖn(1)
-sagÖn(-1)
-SolArka(1)
-sagArka(-1)
-basic.forever(function () {
-	
-})
